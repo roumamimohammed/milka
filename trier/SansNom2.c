21c6914d636650cbc7929e0cb5ec9a5665901a19
¨roumamimohammed¨ <roumamimohammed2@gmail.com>
2022-09-08 15:07:08 +0100

    secand commit

--- trier/SansNom2.c
+++ trier/SansNom2.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main() {
 	
-	int i,j,n,T[100];
-
+	int i,j,tmp,n,T[100];
      	printf("entrer le nombres des eleves: ");
 	    char  in[255];
 	    fgets(in,255,stdin);
@@ -27,9 +24,18 @@ int main() {
 			goto renter;
 		}
 	}
-	
 	for(i=0;i<n;i++){
-		printf("les notes sonts eleve:%d: %d\n",i+1,T[i]);
+		for(j=i+1;j<=n;j++){
+			if(T[j]<T[i]){
+			tmp=T[i];
+			T[i]=T[j];
+			T[j]=tmp	;
+			}	
+		
+	}	
+	}
+	for(i=0;i<n;i++){
+		printf("les notes sonts eleve:%d: %d\n",i+1,T[i+1]);
 	}
 	
 	return 0;
